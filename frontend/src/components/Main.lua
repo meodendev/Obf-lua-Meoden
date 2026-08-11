c2815b28612d13f3837ea037bf4e377936406163
meodendev <meoden.dev@gmail.com>
2026-08-11 12:18:19 +0700

    Create Main.lua

---
+++ frontend/src/components/Main.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Key = "cedcbeba78ef4337731444a0" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
